65b2436bfa7ea98c3cf4ab221ae49988ed9401ce
duan.ho-van <duan.ho-van@banvien.com.vn>
2023-08-17 11:20:03 +0700

    x

---
+++ src/test/java/com/demo/coder/CoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.coder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
