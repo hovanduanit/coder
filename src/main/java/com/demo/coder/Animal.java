31e3193c90e70583de66393498c2babc39fa82dd
hovanduanit <hovanduanit@gmail.com>
2023-09-25 22:34:38 +0700

    add animal8

--- src/main/java/com/demo/coder/Animal.java
+++ src/main/java/com/demo/coder/Animal.java
@@ -6,4 +6,7 @@ public class Animal {
     private String animal3;
     private String animal4;
     private String animal5;
+    private String animal6;
+    private String animal7;
+    private String animal8;
 }
